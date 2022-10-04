9ff735ccaeb9bd964ecc514394a77f6e37b20f2a
Anjali Bhardwaj <96973977+AnjaliiBhardwaj@users.noreply.github.com>
2022-10-04 23:51:05 +0530

    Add files via upload

---
+++ Pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    int temp =n;
+    while(temp > 0){
+    	for(int i = 0; i < n; i++ ){
+    		cout<< n;
+		}
+		cout<<endl;
+		temp--;
+	}
+  
+}
