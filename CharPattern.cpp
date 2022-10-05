e13f13d47bd8835c836f45d230e87a2b1e928157
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2022-10-05 15:48:56 +0530

    Char Pattern

---
+++ CharPattern.cpp
@@ -0,0 +1,26 @@
+/*
+A
+BC
+CDE
+DEFG
+EFGHI
+FGHIJK
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<char('A'+i+j-2);
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
