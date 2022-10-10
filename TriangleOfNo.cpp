e7374fa54afabf22b33a2e3d0d3fd6666faf3b5a
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2022-10-10 23:11:45 +0530

    Updated

---
+++ TriangleOfNo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*  Read input as specified in the question.
+     * Print output as specified in the question.
+     */
+    int n;
+    cin>>n;
+    
+    int i=1;
+    while(i <= n) {
+        
+        for(int sp = 1 ;sp <= n - i; sp++){
+            cout << " ";
+        }
+        int temp = i;
+        for(int k = 1; k <= i; k++){
+            cout << temp++;
+        }
+        temp--;
+        for(int j = 1; j < i; j++){
+            cout << --temp;
+        }
+        cout << endl;
+        i++;
+    }
+    
+}
