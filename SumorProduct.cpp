5a7c450860a1da044683f9c4da1661b57a189a0f
Anjali Bhardwaj <96973977+AnjaliiBhardwaj@users.noreply.github.com>
2022-10-11 23:19:55 +0530

    Add files via upload

---
+++ SumorProduct.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n,q;
+	cin>>n>>q; 
+    int sum=0; 
+   
+    int prod=1; 
+    for(int i=1;i<=n;i++){ 
+        if (q==1){ 
+            sum = sum +i; 
+        }  
+        else { 
+            prod =prod * i; 
+        } 
+    } 
+    if (q ==1){ 
+         cout<<sum<<endl; 
+    } 
+    else if(q==2){ 
+     cout<<prod<<endl; 
+    }
+    else{
+        cout<<-1;
+    }
+}
